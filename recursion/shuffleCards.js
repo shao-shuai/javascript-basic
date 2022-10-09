fbb6954085528dd715c4eda8a41889d7cba71a0c
shao-shuai <shuai-shao@outlook.com>
2022-10-08 23:55:07 -0700

    shuffle cards

---
+++ recursion/shuffleCards.js
@@ -0,0 +1,21 @@
+function shuffleCards(topHalf, bottomHalf) {
+  // YOUR CODE HERE
+}
+
+// UNCOMMENT TO TEST YOUR WORK
+const topHalf = [
+  "Queen of Diamonds",
+  "Five of Hearts",
+  "Ace of Spades",
+  "Eight of Clubs",
+];
+const bottomHalf = ["Jack of Hearts", "Ten of Spades"];
+console.log(shuffleCards(topHalf, bottomHalf));
+/*-> ['Queen of Diamonds',
+          'Jack of Hearts',
+          'Five of Hearts',
+          'Ten of Spades',
+          'Ace of Spades',
+          'Eight of Clubs',
+        ]
+    */
