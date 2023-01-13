ac56747607acf6dd057dee9d22abbc6121e1e6b9
shao-shuai <shuai-shao@outlook.com>
2023-01-12 23:10:02 -0800

    update recursion

--- recursion.js
+++ recursion.js
@@ -49,8 +49,22 @@ function palindrome(string) {
 // console.log(palindrome("jmoney")); //-> false
 
 // Challenge 4
+// the order of 3 if matters
+function isPrime(num, i = 2) {
+  if (num < 2) {
+    return false;
+  }
 
-function isPrime(num) {}
+  if (num === i) {
+    return true;
+  }
+
+  if (num % i === 0) {
+    return false;
+  }
+
+  return isPrime(num, i + 1);
+}
 
 // console.log(isPrime(1)); //-> false
 // console.log(isPrime(2)); //-> true
@@ -59,43 +73,121 @@ function isPrime(num) {}
 
 //Challenge 5
 function pathFinder(obj, arr) {
-  if (arr.length === 1) {
-    return obj[arr];
+  if (arr.length === 0) {
+    return obj;
   }
+
+  const [first, ...rest] = arr;
+  return pathFinder(obj[first], rest);
 }
 
-// const obj = { first: { second: { third: "finish" } }, second: { third: "wrong" } };
+const obj = {
+  first: { second: { third: "finish" } },
+  second: { third: "wrong" },
+};
 // const arr = ["first", "second", "third"];
-// console.log(pathFinder(obj, arr));   //-> "finish"
+// console.log(pathFinder(obj, arr)); //-> "finish"
 
 //Challenge 6
-function flattenRecursively(arr) {}
+function flattenRecursively(arr) {
+  let flattened = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      flattened = flattened.concat(flattenRecursively(arr[i]));
+    } else {
+      flattened.push(arr[i]);
+    }
+  }
+
+  return flattened;
+}
 
 // console.log(flattenRecursively([1, [2, 3, [4]]])); //-> [1, 2, 3, 4]
 // console.log(flattenRecursively([1, {}, [3, [[4]]]])); //-> [1, {}, 3, 4]
 
 //Challenge 7
-function findInOrderedSet(arr, target) {}
+function findInOrderedSet(arr, target) {
+  console.log(arr);
+  if (arr.length === 1) {
+    if (arr[0] === target) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  let n = arr.length;
+  n = Math.floor(n / 2);
+
+  if (target === arr[n]) {
+    console.log(`target is ${target}`);
+    console.log(`middle is ${arr[n]}`);
+    return true;
+  }
+
+  if (target > arr[n]) {
+    return findInOrderedSet(arr.slice(n + 1), target);
+  } else {
+    return findInOrderedSet(arr.slice(0, n), target);
+  }
+}
 
 // const nums = [1, 4, 6, 7, 9, 17, 45];
-// console.log(findInOrderedSet(nums, 4));  //-> true
-// console.log(findInOrderedSet(nums, 2));  //-> false
+// console.log(findInOrderedSet(nums, 4)); //-> true
+// console.log(findInOrderedSet(nums, 2)); //-> false
 
 //Challenge 8
-function countWaysToReachNthStair(n) {}
+function countWaysToReachNthStair(n) {
+  if (n == 1) {
+    return 1;
+  }
 
-// console.log(countWaysToReachNthStair(1)) //-> 1 (only one way to climb 1 stair)
-// console.log(countWaysToReachNthStair(2)) //-> 2 ((1, 1), (2))
-// console.log(countWaysToReachNthStair(4)) //-> 5 ((1, 1, 1, 1), (1, 1, 2), (2, 1, 1), (2, 2))
+  if (n == 2) {
+    return 2;
+  }
+
+  return countWaysToReachNthStair(n - 1) + countWaysToReachNthStair(n - 2);
+}
+
+// console.log(countWaysToReachNthStair(1)); //-> 1 (only one way to climb 1 stair)
+// console.log(countWaysToReachNthStair(2)); //-> 2 ((1, 1), (2))
+// console.log(countWaysToReachNthStair(4)); //-> 5 ((1, 1, 1, 1), (1, 1, 2), (2, 1, 1), (2, 2))
 
 //Challenge 9
-function getPermutations(arr) {}
+function getPermutations(nums) {
+  let result = [];
+  if (nums.length === 0) return [];
+  if (nums.length === 1) return [nums];
+  for (let i = 0; i < nums.length; i++) {
+    const currentNum = nums[i];
+    console.log(`current num is ${currentNum}`);
+    const remainingNums = nums.slice(0, i).concat(nums.slice(i + 1));
+    console.log(`remaind nums is ${remainingNums}`);
+    const remainingNumsPermuted = getPermutations(remainingNums);
 
-// console.log(getPermutations([1, 2])) //-> [[1, 2], [2, 1]]
-// console.log(getPermutations([1, 2, 3])) //-> [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
+    console.log(`remaindnumsper is ${remainingNumsPermuted}`);
+    console.log("----------");
+    for (let j = 0; j < remainingNumsPermuted.length; j++) {
+      const permutedArray = [currentNum].concat(remainingNumsPermuted[j]);
+      result.push(permutedArray);
+      // console.log(result);
+      // console.log("------");
+    }
+  }
+  return result;
+}
+
+// console.log(getPermutations([1, 2])); //-> [[1, 2], [2, 1]]
+// console.log(getPermutations([1, 2, 3])); //-> [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
 
 //Challenge 10
-function getRangeBetween(x, y) {}
+function getRangeBetween(x, y) {
+  if (x + 1 === y) {
+    return [];
+  }
 
-// console.log(getRangeBetween(2, 9)) //-> [3, 4, 5, 6, 7, 8]
-// console.log(getRangeBetween(-5, 5)) //-> [-4, -3, -2, 1, 0, 1, 2, 3, 4]
+  return [x + 1].concat(getRangeBetween(x + 1, y));
+}
+
+console.log(getRangeBetween(2, 9)); //-> [3, 4, 5, 6, 7, 8]
+console.log(getRangeBetween(-5, 5)); //-> [-4, -3, -2, 1, 0, 1, 2, 3, 4]
--- test.js
+++ test.js
@@ -1,7 +1,17 @@
-const arr = [true, true, false, false, true];
-const a = arr.forEach((ele, index) => {
-  if (ele === true) {
-    return index;
+const a = [1, 2, 3, 4, 5];
+
+function eq(a, b) {
+  if (a === b) {
+    return true;
+  } else {
+    return false;
   }
-});
-console.log(a);
+}
+// console.log(eq(4, 4));
+const [first, ...rest] = a;
+console.log(first);
+console.log(rest);
+console.log(Math.abs(10 - 19));
+const arr = [];
+console.log(arr.unshift(10));
+console.log(arr.unshift(2));
